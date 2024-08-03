f8508dbbdc874a6d8c2896a27e7619b9fe3cfc67
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2024-08-03 17:25:57 +0530

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,6 @@ function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 
   return (
     <html lang='en'>
-      <head>
-        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-      </head>
       <Providers>
         <body className={inter.className}>
           {children}
