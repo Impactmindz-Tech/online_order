c671b4dae03d4cc06baff1a162324b72d554e595
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2024-08-03 18:40:05 +0530

    update

--- src/app/components/Multilangage.tsx
+++ src/app/components/Multilangage.tsx
@@ -1,37 +1,42 @@
 "use client";
-import React  from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 import "../../i18n";
 import { getFromLocalStorage, removeFromLocalStorage, setInLocalStorage } from "../utills/LocalStorageUtills";
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { resetCart } from "../store/slice/ProductSlice";
 const Multilangage: React.FC = () => {
+  const dispatch = useDispatch();
   const { t, i18n } = useTranslation();
 
   // TypeScript type for the event
   const changeLanguage = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedLanguage = e.target.value;
-    setInLocalStorage('lang', selectedLanguage); // Store the selected language in localStorage
+    setInLocalStorage("lang", selectedLanguage); // Store the selected language in localStorage
     i18n.changeLanguage(selectedLanguage); // Change the language in i18n
   };
 
   useEffect(() => {
-    const savedLanguage = getFromLocalStorage('lang') || 'en';
+    const savedLanguage = getFromLocalStorage("lang") || "en";
     i18n.changeLanguage(savedLanguage);
-    localStorage.clear();
+    dispatch(resetCart());
+    localStorage.removeItem("location");
+    localStorage.removeItem("categoryProduct");
+    localStorage.removeItem("cartBreakfast");
+    localStorage.removeItem("cartLunch");
+    localStorage.removeItem("categorySelection");
+    localStorage.removeItem("cartDinner");
+    localStorage.removeItem("categorySelection");
   }, []);
   return (
     <div className="absolute right-5 top-5 p-2 rounded-lg shadow-lg border border-gray-300">
-    <select
-      value={i18n.language}
-      onChange={changeLanguage}
-      aria-label="Select Language"
-      className="p-2 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-    >
-      <option value="en">English</option>
-      <option value="he">Hebrew</option>
-      <option value="ru">Russian</option>
-    </select>
-  </div>
+      <select value={i18n.language} onChange={changeLanguage} aria-label="Select Language" className="p-2 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
+        <option value="en">English</option>
+        <option value="he">Hebrew</option>
+        <option value="ru">Russian</option>
+      </select>
+    </div>
   );
 };
 
